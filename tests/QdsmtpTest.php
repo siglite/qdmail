8a304894a7311c5fc26e5b11e1235450a62f7426
Shun Ishihara <sig@siglite.net>
2019-01-06 23:18:57 +0900

    Add minimum test (with travis-ci) (#1)

---
+++ tests/QdsmtpTest.php
@@ -0,0 +1,14 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require_once "./qdsmtp.php";
+
+class QdsmtpTest extends TestCase
+{
+    public function testQdsmtp()
+    {
+        $instance = new Qdsmtp();
+
+        # Todo: Implement debug function.
+    }
+}
